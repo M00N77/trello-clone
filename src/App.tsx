e56c2db354c40ec0986b6fa533f17fa0ba87e1cc
Platon <kplatonglfc@gmail.com>
2026-01-17 17:27:25 +0300

    feat: add initial layout for Trello columns and tasks

---
+++ src/App.tsx
@@ -0,0 +1,146 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <header className="flex  justify-center items-center">
+        <div>
+          <nav>
+            <ul className="flex gap-x-2">
+
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+              <li><a href="">Задачи</a></li>
+
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main className="flex justify-center items-center">
+        <div>
+          <div>main_header</div>
+
+          <div className="flex gap-x-10">
+
+            <div className=" flex flex-col border w-72  shrink-0">
+              <h2 className="bg-red-400 text-center px-3 py-2 font-bold" >To Do</h2>
+              <div>
+                <ul className="flex flex-col gap-y-4 ">
+
+                  <li className="">
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+
+            <div className=" flex flex-col border w-72  shrink-0">
+              <h2 className="bg-blue-400 text-center px-3 py-2 font-bold" >In Progres</h2>
+              <div>
+                <ul className="flex flex-col gap-y-4 ">
+
+                  <li className="">
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+
+
+            <div className=" flex flex-col border w-72  shrink-0">
+              <h2 className="bg-green-400 text-center px-3 py-2 font-bold" >Done</h2>
+              <div>
+                <ul className="flex flex-col gap-y-4 ">
+
+                  <li className="">
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="shrink-1 border rounded-md bg-slate-100 px-2 py-4">
+                      <h3 className="font-bold" >Task_1</h3>
+
+                      <p className="inline-flex p-1 rounded-lg text-red-500  bg-red-100 text-xs font-semibold" >19 часов</p>
+                    </div>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+
+            
+
+            
+          </div>
+        </div>
+      </main>
+
+    </>
+  )
+}
+
+export default App
